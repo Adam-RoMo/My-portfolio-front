57b2d64304d82b9a32fdcffb3ba4faff562dc853
Adam <adam.robert.motta@gmail.com>
2025-05-19 10:18:12 +0200

    🚨 Fix linter warning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h1 className="text-4xl font-bold">Bienvenue sur mon portfolio !</h1>
-            <p className="mt-4 text-lg">Vous trouverez ici mes derniers projets destiné pour la plupart a des fin d'apprentissage.</p>
+            <p className="mt-4 text-lg">Vous trouverez ici mes derniers projets destiné pour la plupart a des fin d&apos;apprentissage.</p>
         </main>
     );
 }
